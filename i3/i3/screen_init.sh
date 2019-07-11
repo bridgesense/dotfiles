28f6c653871e86f274a78f4e89f13adad1b3ab39
Francis Meetze <francis@localhost.localdomain>
2019-07-11 13:25:16 -0700

    screen controls update

--- i3/i3/screen_init.sh
+++ i3/i3/screen_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash -c "~/.screenlayout/default.sh"
 xrandr --auto
-xrandr --output DP2-2 --mode 1920x1080 --left-of eDP1
+xrandr --output DP2-2 --primary --mode 1920x1080 --left-of eDP1
 feh --randomize --bg-scale ~/Nextcloud/backgrounds/*
