9235e307f2dd96b514d8cb40c8a99ce18b8cda6a
Francis Meetze <francis@localhost.localdomain>
2019-07-23 15:06:00 -0700

    Varias example updates

--- i3/i3/screen_init.sh
+++ i3/i3/screen_init.sh
@@ -3,3 +3,4 @@ bash -c "~/.screenlayout/default.sh"
 xrandr --auto
 xrandr --output DP2-2 --primary --left-of eDP1
 feh --randomize --bg-scale ~/Nextcloud/backgrounds/*
+setxkbmap -option caps:swapescape
