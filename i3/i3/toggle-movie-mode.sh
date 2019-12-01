c0a2a998da28f09651da9eef8e03b5e1eef99dfb
Francis Meetze <francis@localhost.localdomain>
2019-12-01 15:32:54 -0800

    i3 Workstation Update

--- i3/i3/toggle-movie-mode.sh
+++ i3/i3/toggle-movie-mode.sh
@@ -3,9 +3,11 @@
 if xset q | grep -q "timeout:  0" ; then
     xautolock -enable
     xset s on
-    notify-send -i network-wireless-full "Movie Mode OFF" "The Screen Lock and Saver have been enabled."
+    xset +dpms
+    notify-send -i "Movie Mode OFF" "The Screen Lock and Saver have been enabled."
 else
     xautolock -disable
     xset s off
-    notify-send -i network-wireless-disconnected "Movie Mode ON" "The Screen Lock and Saver have been disabled."
+    xset -dpms
+    notify-send -i "Movie Mode ON" "The Screen Lock and Saver have been disabled."
 fi
