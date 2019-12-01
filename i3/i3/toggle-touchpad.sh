5ad34a1720bfedbb162c0216acd457f13d413cb1
Francis Meetze <francis@localhost.localdomain>
2019-12-01 15:32:14 -0800

    i3 Workstation Update

---
+++ i3/i3/toggle-touchpad.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if xinput list-props "SynPS/2 Synaptics TouchPad" | grep -n "Device Enabled \(.*\):.*1" ; then
+    xinput disable "SynPS/2 Synaptics TouchPad"
+    notify-send -i "TouchPad Disabled" "Your laptop's touchpad has been enabled."
+else
+    xinput enable "SynPS/2 Synaptics TouchPad"
+    notify-send -i "Movie Mode ON" "Your laptop's touchpad has been disabled."
+fi
