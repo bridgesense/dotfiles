4835a19222e74a205fade6b645e8b7b80b53d140
Francis Meetze <francis@localhost.localdomain>
2020-01-27 11:26:45 -0800

    Workstation update

--- shared/scripts/battery.sh
+++ shared/scripts/battery.sh
@@ -3,7 +3,7 @@
 # include the following line
 # */15 * * * * /bin/bash /home/francis/.config/i3/battery.sh >/dev/null 2>&1
 
-BATTINFO=`acpi -b | head -n 1`
+BATTINFO=`acpi -b | tail -n 1`
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 ]] ;
 then
