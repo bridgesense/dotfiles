bf1a9cea3c964828883a8be207552d44d8150466
Francis Meetze <francis@bridgesense.com>
2020-03-27 23:11:39 -0700

    Workstation update

--- shared/scripts/shutdown.sh
+++ shared/scripts/shutdown.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$SHELL -c `if zenity --question --text="Are you sure you want to power down?"; then shutdown -h now; fi`
+$SHELL -c `if zenity --question --text="Are you sure you want to power down?"; then shutdown -h now; fi`;
